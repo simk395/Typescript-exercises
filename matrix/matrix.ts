5042a53d757483bf7189f60910ea237705f59cf4
Simon Kong <simon.kong95@gmail.com>
2019-10-23 14:47:44 -0400

    matrix

--- matrix/matrix.ts
+++ matrix/matrix.ts
@@ -1,11 +1,25 @@
 class Matrix {
   rows: number[][];
   columns: number[][];
-  constructor(row: string = '', cols: string = '') {
-    this.rows = row
-      .split('\n')
-      .map(num => num.split(' ').map(num2 => parseInt(num2)));
-    this.columns = new Array(cols.split('\n').map(num => parseInt(num)));
+  constructor(matrix: string) {
+    this.rows = this.getRows(matrix)
+    this.columns = this.getColumns(matrix)
+  }
+
+  getRows(matrix: string) {
+    return matrix.split('\n')
+      .map(num => num
+        .split(' ')
+        .map(num2 => parseInt(num2)));
+  }
+
+  getColumns(matrix: string) {
+    const rows = this.getRows(matrix);
+    const cols = new Array(rows.length);
+    for (let i = 0; i < rows.length; i++) {
+      cols[i] = rows.map(row => row[i]);
+    }
+    return cols;
   }
 }
 
