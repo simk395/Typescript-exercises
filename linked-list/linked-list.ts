804f682794197cfda036a7fbd0a0bd4619758ede
Simon Kong <simon.kong95@gmail.com>
2019-10-31 23:26:17 -0400

    linked list incomplete

---
+++ linked-list/linked-list.ts
@@ -0,0 +1,18 @@
+export default class LinkedList {
+    value: number;
+    constructor(num: number) {
+        this.value = num;
+    }
+    push(num: number) {
+
+    }
+    pop(num: number) {
+
+    }
+    shift(num: number) {
+
+    }
+    unshift(num: number) {
+
+    }
+}
